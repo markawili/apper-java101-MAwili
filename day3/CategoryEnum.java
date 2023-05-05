e996fd6a8d9489c2c835a820f261d15d2c6b71c1
Mark Awili <markandrew.awili@gmail.com>
2023-05-05 12:08:54 +0800

    Created Switch Statement for Category enum

---
+++ day3/CategoryEnum.java
@@ -0,0 +1,29 @@
+package day3;
+
+public class CategoryEnum {
+    public static void main(String[] args) {
+        Category category = Category.GRANDPA;
+
+        switch(category) {
+            case MEN: System.out.println("Price is " + Category.MEN.getPrice()); break;
+            case WOMEN: System.out.println("Price is " + Category.WOMEN.getPrice()); break;
+            case KID: System.out.println("Price is " + Category.KID.getPrice()); break;
+            case GRANDMA: System.out.println("Price is " + Category.GRANDMA.getPrice()); break;
+            case GRANDPA: System.out.println("Price is " + Category.GRANDPA.getPrice()); break;
+        }
+
+    }
+
+    public enum Category {
+        MEN(13.57), WOMEN(20.45), KID(45.6), GRANDMA(56.67), GRANDPA(3.56);
+        private double price;
+
+        Category(double price) {
+            this.price = price;
+        }
+
+        public double getPrice() {
+            return price;
+        }
+    }
+}
