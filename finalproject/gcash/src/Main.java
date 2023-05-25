6e8543d14cbe4e69af813cbe8b182e08ce9328cc
Mark Awili <markandrew.awili@gmail.com>
2023-05-25 09:00:43 +0800

    Final project NoCash v1

---
+++ finalproject/gcash/src/Main.java
@@ -0,0 +1,99 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    private static Map<String, User> accounts = new HashMap<String, User>();
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        initializeAccounts();
+
+        boolean repeat = true;
+
+        while (repeat) {
+            System.out.println("============ Welcome to JeeCash-Mini! ============");
+            System.out.println("\nSelect from the options what you want to do:");
+            System.out.println("[1] Register a new account");
+            System.out.println("[2] Send load");
+            System.out.println("[3] Display balances");
+            System.out.print("\nWhat do you want to do?: ");
+
+            int transactionChoice = scanner.nextInt();
+            String repeatChoice = "";
+
+            switch(transactionChoice) {
+                case 1:
+                    System.out.println("============ Register Account ============");
+                    try {
+                        User newUser = User.createUser(accounts);
+                        accounts.put(newUser.getNumber(), newUser);
+                    } catch (Exception e) {
+                        System.out.println("Cannot create user!");;
+                    }
+
+                    System.out.print("Do you like to do another transaction? [Y/N]: ");
+                    repeatChoice = scanner.next();
+                    if(repeatChoice.equalsIgnoreCase("N")) {
+                        System.out.println("Thank you for using NoCash!");
+                        repeat = false;
+                    }
+                    System.out.println("");
+                    break;
+                case 2:
+                    System.out.println("============ Send Load ============");
+                    User.shareLoad(accounts);
+                    try {
+
+                    } catch (Exception e) {
+                        System.out.println("Cannot create user!");;
+                    }
+
+                    System.out.print("Do you like to do another transaction? [Y/N]: ");
+                    repeatChoice = scanner.next();
+                    if(repeatChoice.equalsIgnoreCase("N")) {
+                        System.out.println("Thank you for using NoCash!");
+                        repeat = false;
+                    }
+                    System.out.println("");
+                    break;
+                case 3:
+                    System.out.println("============ Display Balances ============");
+                    accounts.forEach((key, value) -> {
+                        System.out.println("Number: " + key + " Name: " + value.getName() + " Balance: " + value.getBalance());
+                    });
+
+                    System.out.print("Do you like to do another transaction? [Y/N]: ");
+                    repeatChoice = scanner.next();
+                    if(repeatChoice.equalsIgnoreCase("N")) {
+                        System.out.println("Thank you for using NoCash!");
+                        repeat = false;
+                    }
+                    System.out.println("");
+                    break;
+                default:
+                    System.out.print("Invalid choice. Do you like to try again? [Y/N]: ");
+                    repeatChoice = scanner.next();
+                    if(repeatChoice.equalsIgnoreCase("N")) {
+                        System.out.println("Thank you for using NoCash!");
+                        repeat = false;
+                    }
+                    System.out.println("");
+            }
+        }
+    }
+
+    private static void initializeAccounts() {
+        User user1 = new User("09175861661", "Bob");
+        User user2 = new User("09175861662", "Marley");
+        User user3 = new User("09175861663", "Seth");
+        User user4 = new User("09175861664", "Ryan");
+        User user5 = new User("09175861665", "Fritz");
+
+        accounts.put(user1.getNumber(), user1);
+        accounts.put(user2.getNumber(), user2);
+        accounts.put(user3.getNumber(), user3);
+        accounts.put(user4.getNumber(), user4);
+        accounts.put(user5.getNumber(), user5);
+    }
+}
---
+++ finalproject/gcash/src/User.java
@@ -0,0 +1,109 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+interface Account {
+    public static void shareLoad(Map<String, User> accounts) {}
+    public User registerAccount(Map<String, User> account);
+}
+public class User implements Account{
+    private String number;
+    private String name;
+    private double balance;
+
+    User(String number, String name) {
+        this.number = number;
+        this.name = name;
+        this.balance = 100;
+    }
+
+    public static void shareLoad(Map<String, User> accounts) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Share-a-Load");
+
+        System.out.print("Enter Sender's Number: ");
+        String senderNumber = scanner.nextLine();
+        System.out.print("Enter Recipient's Number: ");
+        String recipientNumber = scanner.nextLine();
+
+        if (senderNumber.equals(recipientNumber)) {
+            System.out.println("Sender's Number must not be the same as the Recipient's");
+            return;
+        } else if (!accounts.containsKey(senderNumber)) {
+            System.out.println("Sender's Number is not registered");
+            return;
+        } else if (!accounts.containsKey(recipientNumber)) {
+            System.out.println("Recipient's Number is not registered");
+            return;
+        }
+
+        User sender = accounts.get(senderNumber);
+        User recipient = accounts.get(recipientNumber);
+
+        System.out.print("Enter amount you want to send: ");
+        double amountToSend = scanner.nextDouble();
+        if (amountToSend > sender.getBalance()) {
+            System.out.println("Insufficient balance.");
+            return;
+        } else {
+            sender.setBalance((sender.getBalance() - amountToSend));
+            recipient.setBalance((recipient.getBalance() + amountToSend));
+        }
+        System.out.println("Transfer completed!");
+    }
+
+    public User registerAccount(Map<String, User> accounts) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Register account");
+
+        System.out.print("Enter number: ");
+        String number = scanner.nextLine();
+        if (number.length() != 11) {
+            System.out.println("Invalid number");
+            return null;
+        } else if (number.isEmpty()) {
+            System.out.println("Number is empty");
+            return null;
+        } else if (accounts.containsKey(number)) {
+            System.out.println("Number exists");
+            return null;
+        }
+
+        System.out.print("Enter name: ");
+        String name = scanner.nextLine();
+        if (name.isEmpty()) {
+            System.out.println("Name is empty");
+            return null;
+        }
+        return new User(number, name);
+    }
+
+    public static User createUser(Map<String, User> accounts) {
+        User newUser = new User("", "");
+        return newUser.registerAccount(accounts);
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
